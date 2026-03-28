99a68dd88b08c6a8dec4028a1fac45f5d3bfc9b1
hafzism <hafeezpallath@gmail.com>
2026-03-29 01:45:55 +0530

    feat(api): integrate lists and cards modules into app module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -7,6 +7,8 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
 import { BoardsModule } from './boards/boards.module';
+import { ListsModule } from './lists/lists.module';
+import { CardsModule } from './cards/cards.module';
 
 @Module({
   imports: [
@@ -16,6 +18,8 @@ import { BoardsModule } from './boards/boards.module';
     AuthModule,
     WorkspacesModule,
     BoardsModule,
+    ListsModule,
+    CardsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
